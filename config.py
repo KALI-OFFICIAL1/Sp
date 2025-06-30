e5f1d26e4f4b4e074f0a491ddc0fba8823e61cd5
KALI-OFFICIAL1 <kaliofficial448@gmail.com>
2025-06-30 10:47:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,6 +41,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", "30DxNexGenBotsd7f15a") # youtube song api key, 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/otploothub")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/frozenTools")
 
